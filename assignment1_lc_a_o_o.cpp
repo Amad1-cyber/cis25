06e57cc1e2407f272fc344aa9dfe3143201cb890
Amad1-cyber <145495116+Amad1-cyber@users.noreply.github.com>
2025-05-14 21:30:54 -0700

    Add files via upload

---
+++ assignment1_lc_a_o_o.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+
+{
+
+    cout << "Hello, world, from Laney C++!" << endl;
+
+    cout << "I Love Programming" << endl;
+
+    return 0;
+
+}
